c328a7a046371f19aa8e8c70dd1781d7ae823f69
Chris Lea <chris.lea@gmail.com>
2015-02-23 21:19:14 -0800

    Use routers to generate our URL conf.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from snippets import views
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('snippets.urls')),
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
 )
