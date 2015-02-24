00891c9a54cea0f4bf4f184994ee463f24a522cf
Chris Lea <chris.lea@gmail.com>
2015-02-23 19:31:49 -0800

    Add enpoint for root of API.

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,9 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
 
@@ -41,3 +44,11 @@ class UserDetail(generics.RetrieveAPIView):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
