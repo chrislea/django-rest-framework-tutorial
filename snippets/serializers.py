df01109c68492ce846f17044ca8fc618ecc3bf15
Chris Lea <chris.lea@gmail.com>
2015-02-23 20:48:18 -0800

    Use hyperlinking for relationships in the API.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,18 +4,21 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight',
+                                                     format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style',
-                  'owner')
+        fields = ('url', 'title', 'code', 'linenos', 'language', 'style',
+                  'owner', 'highlight')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail',
+                                                   read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'username', 'snippets')
